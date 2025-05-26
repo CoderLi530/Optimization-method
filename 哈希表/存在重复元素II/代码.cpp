de95c2711b7928e40616a72a94681aa774a54b01
CoderLi530 <Haopeoplezz@outlook.com>
2025-05-26 11:20:47 +0800

    哈希表

---
+++ 哈希表/存在重复元素II/代码.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        //只有一个元素返回false
+        if (nums.size() == 1) return false;
+
+        //创建哈希表
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            //找到了下标相减与k比较
+            if (hash.count(nums[i]) && i - hash[nums[i]] <= k) return true;
+            hash[nums[i]] = i;
+        }        
+        //遍历结束说明没找到
+        return false;
+    }
+};
