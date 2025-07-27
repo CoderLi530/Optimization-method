51381112675c6e332c05180eef387a46389a9b80
CoderLi530 <Haopeoplezz@outlook.com>
2025-07-27 22:03:28 +0800

    环形链表

--- 链表/链表的基础与总结/环形链表/代码.cpp
+++ 链表/链表的基础与总结/环形链表/代码.cpp
@@ -1,3 +1,5 @@
+//题目链接：https://leetcode.cn/problems/linked-list-cycle/?envType=study-plan-v2&envId=top-100-liked
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -6,20 +8,39 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+class Solution 
+{
 public:
-    bool hasCycle(ListNode *head) {
-        //数组为空返回false
-        if (head == NULL) return false;
-        ListNode* fast = head;//快指针
-        ListNode* slow = head;//慢指针
-        //遍历整个链表
-        while (slow->next != NULL && fast->next != NULL &&fast->next->next != NULL)//注意节点为空的情况
+    bool hasCycle(ListNode *head) 
+    {
+        // //1. 快慢指针，fast比slow多走一步，如果有环一定会相遇
+        // //时间复杂度O(n) 空间复杂度O(1)
+        // if (head == nullptr || head->next == nullptr) return false;
+        // ListNode* fast = head;
+        // ListNode* slow = head;
+        // while (fast->next && fast->next->next)
+        // {
+        //     fast = fast->next->next;
+        //     slow = slow->next;
+        //     //相遇返回true
+        //     if (fast == slow) return true;
+        // }
+        // //链表中没有环，返回false
+        // return false;
+
+        //2. 哈希表，遍历链表时在哈希表中查找，找到了说明有环
+        //时间复杂度O(n) 空间复杂度O(n)
+        unordered_set<ListNode*> hash;
+        ListNode* cur = head;
+        while (cur)
         {
-            fast = fast->next->next;
-            slow = slow->next;
-            if (fast == slow)   return true;//节点相等有环
+            //找到了直接返回
+            if (hash.count(cur)) return true;
+            //没找到，存入哈希表中
+            hash.insert(cur);
+            cur = cur->next;
         }
+        //说明没有环，直接遍历空了
         return false;
     }
 };
